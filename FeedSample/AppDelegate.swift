fbd7905533f1cea4fddfa9ee8a1dc8314964ce12
marunomi <12097559+marunomi@users.noreply.github.com>
2020-09-15 15:53:29 +0900

    remove storyboard

--- FeedSample/AppDelegate.swift
+++ FeedSample/AppDelegate.swift
@@ -7,6 +7,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = FeedViewController(transitionStyle: .scroll, navigationOrientation: .horizontal, options: [UIPageViewController.OptionsKey.interPageSpacing: 20])
+        window?.makeKeyAndVisible()
         return true
     }
 
